e321f6b622086003065b5dbc4817bbc465484c7f
Tom French <t.french@sbs-claims.co.uk>
2021-11-14 16:16:28 +0000

    DirectoryManager Interface and Class to mock `InitCommand`

---
+++ Wit.App/Services/DirectoryManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wit.Services
+{
+    public class DirectoryManager : IDirectoryManager
+    {
+        public bool Exists(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public DirectoryInfo Create(string path)
+        {
+            return Directory.CreateDirectory(Path.GetFullPath(path));
+        }
+
+        public void Move(string sourceDirName, string destDirName)
+        {
+            Directory.Move(sourceDirName, destDirName);
+        }
+
+        public IEnumerable<DirectoryInfo> EnumerateDirectories(string path, string searchPattern)
+        {
+            var dir = new DirectoryInfo(path);
+            return dir.EnumerateDirectories(searchPattern);
+        }
+
+    }
+}
---
+++ Wit.App/Services/IDirectoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wit.Services
+{
+    public interface IDirectoryManager
+    {
+        bool Exists(string path);
+        DirectoryInfo Create(string path);
+        void Move(String sourceDirName, String destDirName);
+        IEnumerable<DirectoryInfo> EnumerateDirectories(string path, string searchPattern);
+
+    }
+}
