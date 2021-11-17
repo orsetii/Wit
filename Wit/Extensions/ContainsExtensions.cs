73cadf82a59cc92c2a481cc9cbdeb83e1c43bb46
Tom French <t.french@sbs-claims.co.uk>
2021-11-17 14:27:19 +0000

    Extension method to find if matches any in list

---
+++ Wit/Extensions/ContainsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wit.Extensions
+{
+    public static class ContainsExtensions
+    {
+        public static bool ContainsAnyInList(this String str, List<string> list)
+        {
+            foreach (var item in list)
+            {
+                if (str == item)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
