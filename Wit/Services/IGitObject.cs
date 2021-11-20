392400fed72be9d3e299821e946dbb10a396c2dd
Tom French <t.french@sbs-claims.co.uk>
2021-11-20 15:42:37 +0000

    Convert `GitType` to be an enum-like class so we can easily serialize correctly for object storage

--- Wit/Services/IGitObject.cs
+++ Wit/Services/IGitObject.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Wit.Services
 {
     public interface IGitObject
     {
-        public string GitType { get; }
+        public GitObjectType GitType { get; }
         public byte[] Data { get; }
     }
+
+    public class GitObjectType
+    {
+        private GitObjectType(string value) { Value = value; }
+
+        public string Value { get; }
+
+        public static GitObjectType Blob => new("blob");
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+
 }
