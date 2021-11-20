88e262024661925b4221a87d001ad1e4afba0296
Tom French <t.french@sbs-claims.co.uk>
2021-11-20 11:53:25 +0000

    Find Git directory automatically

--- Wit/Utils/Workspace.cs
+++ Wit/Utils/Workspace.cs
@@ -14,39 +14,89 @@ namespace Wit.Utils
         // and change the parsing behaviour to ignore differently as each path on linux uses '.' extensively.
         private readonly List<string> IGNORE_LIST = new List<string> { ".git" };
 
-        private string _pathName;
+        /// <summary>
+        /// The maximum amount of times to traverse upwards when attempting to find a git directory
+        /// </summary>
+        private const int MAX_SEARCH_DEPTH = 5;
+
+        private string _path = String.Empty;
+
+        public string PathName
+        {
+            get { return _path; }
+            set
+            {
+                if (!_directoryManager.Exists(value))
+                    throw new ArgumentException("supplied path does not exist");
+
+                _path = value;
+            }
+        }
 
         private IDirectoryManager _directoryManager;
 
         public Workspace(string pathname)
         {
             _directoryManager = new DirectoryManager();
-            if (!_directoryManager.Exists(pathname))
-                throw new ArgumentException("Supplied path does not exist");
 
 
-            _pathName = pathname;
+            PathName = pathname;
+        }
+        public Workspace()
+        {
+            _directoryManager = new DirectoryManager();
+
+            PathName = FindRootGitDirectory();
         }
 
         public Workspace(string pathName, IDirectoryManager directoryManager)
         {
             _directoryManager = directoryManager;
 
-            if (!_directoryManager.Exists(pathName))
-                throw new ArgumentException("Supplied path does not exist");
 
-            _pathName = pathName;
+            PathName = pathName;
         }
 
         public List<string> ListFiles()
         {
-            return Directory.GetFiles(_pathName, "*.*", SearchOption.AllDirectories)
+            return Directory.GetFiles(PathName, "*.*", SearchOption.AllDirectories)
                 .Where(x => !x.Substring(2).Split("\\")[1].ContainsAnyInList(IGNORE_LIST)).ToList();
         }
 
         public byte[] ReadFile(string path)
         {
-            return _directoryManager.Read(String.Join(_pathName, path));
+            return _directoryManager.Read(String.Join(PathName, path));
         }
+
+        private string FindRootGitDirectory()
+        {
+            var path = ".";
+            var i = 0;
+            while (i <= MAX_SEARCH_DEPTH)
+            {
+                if (_directoryManager.EnumerateDirectories(path, ".git").FirstOrDefault() == null)
+                {
+                    path = "..\\" + path;
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    // Attempt to use the full path, but if not we use the relative path
+                    try
+                    {
+                        return Path.GetFullPath(path);
+                    }
+                    catch (Exception)
+                    {
+                        return path;
+                    }
+                }
+            }
+
+            throw new Exception($"Unable to find a git repository, searched {MAX_SEARCH_DEPTH} upwards");
+        }
+
+
     }
 }
