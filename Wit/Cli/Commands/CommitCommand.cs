8c45e53c2d54ff562c46ffd926f94a1122cfa715
Tom French <t.french@sbs-claims.co.uk>
2021-11-20 22:25:43 +0000

    Namespace fix

--- Wit/Cli/Commands/CommitCommand.cs
+++ Wit/Cli/Commands/CommitCommand.cs
@@ -4,86 +4,85 @@ using Wit.Core;
 using Wit.Utils;
 using Wit.Data;
 
-namespace Wit.Cli.Commands
+namespace Wit.Cli.Commands;
+
+public class CommitCommand : ICliCommand
 {
-    public class CommitCommand : ICliCommand
+
+    private readonly string _rootPath;
+
+    private string _gitPath => Path.Combine(_rootPath, ".git");
+
+    private string _dbPath => Path.Combine(_gitPath, "objects");
+
+    private List<string> _fileList = new();
+
+    private Workspace _ws;
+
+    private Database _db;
+
+    private Author _author;
+
+
+
+    /// <summary>
+    /// Finds the root git directory by traversing upwards from the current directory
+    /// </summary>
+    public CommitCommand()
     {
+        _ws = new Workspace();
 
-        private readonly string _rootPath;
+        _rootPath = _ws.PathName;
+        _db = new Database(_dbPath);
+    }
 
-        private string _gitPath => Path.Combine(_rootPath, ".git");
+    /// <summary>
+    /// Explicitly set the root path for the workspace
+    /// </summary>
+    public CommitCommand(string rootPath)
+    {
+        _rootPath = rootPath;
 
-        private string _dbPath => Path.Combine(_gitPath, "objects");
+        _ws = new Workspace(_rootPath);
+        _db = new Database(_dbPath);
+    }
 
-        private List<string> _fileList = new();
+    public Command CreateCommand()
+    {
+        var cmd = new Command("commit", "Commit changes to the repository.");
+        cmd.AddOption(new Option("--author-email", "The name of the author of this commit"));
+        cmd.AddOption(new Option("--author-name", "The name of the author of this commit"));
+        cmd.AddOption(new Option("--message", "Commit Message"));
 
-        private Workspace _ws;
+        cmd.Handler = CommandHandler.Create(Handle);
 
-        private Database _db;
-
-        private Author _author;
-
-
-
-        /// <summary>
-        /// Finds the root git directory by traversing upwards from the current directory
-        /// </summary>
-        public CommitCommand()
-        {
-            _ws = new Workspace();
-
-            _rootPath = _ws.PathName;
-            _db = new Database(_dbPath);
-        }
-
-        /// <summary>
-        /// Explicitly set the root path for the workspace
-        /// </summary>
-        public CommitCommand(string rootPath)
-        {
-            _rootPath = rootPath;
-
-            _ws = new Workspace(_rootPath);
-            _db = new Database(_dbPath);
-        }
-
-        public Command CreateCommand()
-        {
-            var cmd = new Command("commit", "Commit changes to the repository.");
-            cmd.AddOption(new Option("--author-email", "The name of the author of this commit"));
-            cmd.AddOption(new Option("--author-name", "The name of the author of this commit"));
-            cmd.AddOption(new Option("--message", "Commit Message"));
-
-            cmd.Handler = CommandHandler.Create(Handle);
-
-            return cmd;
-        }
+        return cmd;
+    }
 
 
-        public void Handle(string? authorEmail, string? authorName, string? message)
-        {
-            _author = new Author(authorName, authorEmail);
+    public void Handle(string? authorEmail, string? authorName, string? message)
+    {
+        _author = new Author(authorName, authorEmail);
             
 
 
-            _fileList = _ws.ListFiles();
+        _fileList = _ws.ListFiles();
             
-            var tree = new Tree();
+        var tree = new Tree();
             
             
-            foreach (var path in _fileList)
-            {
-                var data = _ws.ReadFile(path);
-                var blob = new Blob(data);
+        foreach (var path in _fileList)
+        {
+            var data = _ws.ReadFile(path);
+            var blob = new Blob(data);
 
-                _db.Store(blob);
+            _db.Store(blob);
 
-                tree.Entries.Add(new Entry(path, blob.Oid));
-            }
-            _db.Store(tree);
-
+            tree.Entries.Add(new Entry(path, blob.Oid));
         }
-        
+        _db.Store(tree);
 
     }
+        
+
 }
