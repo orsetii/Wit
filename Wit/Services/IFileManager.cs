6ecc29bb0692164e18d3cc1a98e44b403e482d37
Tom French <t.french@sbs-claims.co.uk>
2021-11-20 15:40:34 +0000

    `IFileManager` class to be finished later for mocking use.

---
+++ Wit/Services/IFileManager.cs
@@ -0,0 +1,9 @@
+namespace Wit.Services
+{
+    // TODO flesh out this interface to perform FS operations so we can mock stuff.
+    // In Database for example.
+    public interface IFileManager
+    {
+
+    }
+}
